311994f2c64e7b86acd5bba4866562a9b014fa76
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-27 00:10:55 +0530

    Navbar Updated

--- travel-europe/src/App.jsx
+++ travel-europe/src/App.jsx
@@ -1,12 +1,12 @@
-
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
 
   return (
     <>
-     hi
+     <Navbar/>
     </>
   )
 }
--- travel-europe/src/Components/Navbar.jsx
+++ travel-europe/src/Components/Navbar.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import {useState} from 'react'
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(false);
+
+    const showDropdown = ()=>{
+        setDropdown(!dropdown);
+    };
+
   return (
-    <div>
-      
-    </div>
+    <nav className="w-full h-24 flex flex-col justify-center items-center bg-black fixed z-20">
+        <div className="container mx-auto lg:px-6">
+            <div className="lg:w-full w-11/12 mx-auto h-full flex justify-between items-center">
+                <div className="flex flex-col gap-y-4">
+                    <div className="flex items-center gap-x-4">
+                        <img src={logo} alt="logo image" className='w-12' />
+            
+                        <span>
+                            <p className="text-white font-semibold text-lg leading-tight">Travel</p>
+                            <p className="text-white text-sm">Europe</p>
+                        </span>
+                    </div>
+                </div>
+            
+
+                <ul>
+                    <a href="#home">Home</a>
+                    <a href="#france">France</a>
+                    <a href="#travel">Travel</a>
+                    <a href="#facilities">Facilities</a>
+                    <a href="#tour">Tour</a>
+                    <a href="#blog">Blog</a>
+                    <a href="#gallery">Gallery</a>
+                </ul>
+
+                <div>
+                    <button>Contact Us</button>
+                </div>
+
+                {dropdown ? (
+                    <div onClick={showDropdown}>
+                        <IoMdCloseCircleOutline/>
+                    </div>
+                ) : (
+                    <div onClick={showDropdown}>
+                        <IoMenu/>
+                    </div>
+                )}
+
+            </div>
+
+            {dropdown ? (
+                <div>
+                    <div>
+                        <ul>
+                            <a href="#home">Home</a>
+                            <a href="#france">France</a>
+                            <a href="#travel">Travel</a>
+                            <a href="#facilities">Facilities</a>
+                            <a href="#tour">Tour</a>
+                            <a href="#blog">Blog</a>
+                            <a href="#gallery">Gallery</a>
+                        </ul>
+                        <div>
+                            <button>
+                                Contact Us
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            ) : null}
+        </div>
+       
+    </nav>
   )
 }
 
