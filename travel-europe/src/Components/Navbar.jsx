d86b34432fcc9bf280d97c8b721b19cca0f2c02b
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-28 23:48:07 +0530

    Navbar finished

--- travel-europe/src/Components/Navbar.jsx
+++ travel-europe/src/Components/Navbar.jsx
@@ -27,25 +27,32 @@ const Navbar = () => {
             
 
                 <ul className="flex-1 flex justify-center items-center xl:gap-12 gap-x-4 max-lg:hidden">
-                    <a href="#home" className="leading-normal no-underline text-white text-lg hover:text-primary">Home</a>
-                    <a href="#france" className="leading-normal no-underline text-white text-lg hover:text-primary">France</a>
+                    <a href="#home" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">Home</a>
+
+                    <a href="#france" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">France</a>
+
                     <a href="#travel" className="leading-normal no-underline text-white text-lg hover:text-primary">Travel</a>
-                    <a href="#facilities" className="leading-normal no-underline text-white text-lg hover:text-primary">Facilities</a>
-                    <a href="#tour" className="leading-normal no-underline text-white text-lg hover:text-primary">Tour</a>
-                    <a href="#blog" className="leading-normal no-underline text-white text-lg hover:text-primary">Blog</a>
-                    <a href="#gallery" className="leading-normal no-underline text-white text-lg hover:text-primary">Gallery</a>
+
+                    <a href="#facilities" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">Facilities</a>
+
+                    <a href="#tour" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">Tour</a>
+
+                    <a href="#blog" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">Blog</a>
+
+                    <a href="#gallery" className="leading-normal no-underline text-white text-lg hover:text-primary flex items-center">Gallery</a>
+
                 </ul>
 
-                <div>
-                    <button>Contact Us</button>
+                <div className="flex max-lg:hidden">
+                    <button className="rounded-full border border-solid border-primary bg-transparent texlg text-white px-8 py-3 hoverBtn">Contact Us</button>
                 </div>
 
                 {dropdown ? (
-                    <div onClick={showDropdown}>
+                    <div onClick={showDropdown} className="lg:hidden text-[27px] cursor-pointer text-white">
                         <IoMdCloseCircleOutline/>
                     </div>
                 ) : (
-                    <div onClick={showDropdown}>
+                    <div onClick={showDropdown} className="lg:hidden text-[27px] cursor-pointer text-white">
                         <IoMenu/>
                     </div>
                 )}
@@ -53,16 +60,16 @@ const Navbar = () => {
             </div>
 
             {dropdown ? (
-                <div>
-                    <div>
-                        <ul>
-                            <a href="#home">Home</a>
-                            <a href="#france">France</a>
-                            <a href="#travel">Travel</a>
-                            <a href="#facilities">Facilities</a>
-                            <a href="#tour">Tour</a>
-                            <a href="#blog">Blog</a>
-                            <a href="#gallery">Gallery</a>
+                <div onClick={showDropdown} className="lg:hidden w-full h-[100vh] fixed top-24 bg-black ease-in-out duration-100">
+                    <div className="w-full h-[320px] flex flex-col items-baseline pt-8 gap-4">
+                        <ul className="text-center p-0 flex flex-col justify-center w-full gap-y-8">
+                            <a href="#home" className="leading-normal no-underline text-white text-lg hover:text-primary">Home</a>
+                            <a href="#france" className="leading-normal no-underline text-white text-lg hover:text-primary">France</a>
+                            <a href="#travel" className="leading-normal no-underline text-white text-lg hover:text-primary">Travel</a>
+                            <a href="#facilities" className="leading-normal no-underline text-white text-lg hover:text-primary">Facilities</a>
+                            <a href="#tour" className="leading-normal no-underline text-white text-lg hover:text-primary">Tour</a>
+                            <a href="#blog" className="leading-normal no-underline text-white text-lg hover:text-primary">Blog</a>
+                            <a href="#gallery" className="leading-normal no-underline text-white text-lg hover:text-primary">Gallery</a>
                         </ul>
                         <div>
                             <button>
